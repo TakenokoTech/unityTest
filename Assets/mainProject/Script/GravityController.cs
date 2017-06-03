2bcb53536297ea2ca88a060cad74596959b45db1
Takenoko <michaeelbobu@gmail.com>
2017-06-04 02:42:55 +0900

    add ball game

---
+++ Assets/mainProject/Script/GravityController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityController : MonoBehaviour {
+
+	// 重力加速度
+	const float GRAVITY = 9.8F;
+	// スケールパラメータ
+	public float GRAVITY_SCALE = 1.0F;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 vec = new Vector3 ();
+
+		if (Application.isEditor) {
+			vec.x = Input.GetAxis ("Horizontal");
+			vec.z = Input.GetAxis ("Vertical");
+			vec.y = Input.GetKey ("z") ? 1.0F : -1.0f;
+		} else {
+			vec.x = Input.acceleration.x;
+			vec.y = Input.acceleration.y;
+			vec.z = Input.acceleration.z;
+		}
+
+
+		Physics.gravity = GRAVITY * vec.normalized * GRAVITY_SCALE;
+	}
+}
---
+++ Assets/mainProject/Script/Hole.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hole : MonoBehaviour {
+
+	public string activeTag;
+
+	void OnTriggerStay (Collider other) {
+		Rigidbody r = other.gameObject.GetComponent<Rigidbody> ();
+		Vector3 dir = transform.position - other.gameObject.transform.position;
+		dir.Normalize ();
+
+		if (other.gameObject.tag == activeTag) {
+			r.velocity *= 9.0F;
+			r.AddForce (dir * r.mass * 20.0F);
+		} else {
+			r.AddForce (-dir * r.mass * 80.0F);
+		}
+	} 
+}
